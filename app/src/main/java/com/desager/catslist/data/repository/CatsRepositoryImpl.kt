7658cfbfcae82629469f21059ac986db0b075584
Desager <mr.dan2004@yandex.ru>
2025-11-08 11:17:52 +0300

    Change context of repository methods

--- app/src/main/java/com/desager/catslist/data/repository/CatsRepositoryImpl.kt
+++ app/src/main/java/com/desager/catslist/data/repository/CatsRepositoryImpl.kt
@@ -8,8 +8,10 @@ import com.desager.catslist.data.mapper.CatModelMapper
 import com.desager.catslist.data.remote.api.CatsApi
 import com.desager.catslist.domain.model.CatModel
 import com.desager.catslist.domain.repository.CatsRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
 class CatsRepositoryImpl(
     private val api: CatsApi,
@@ -18,43 +20,51 @@ class CatsRepositoryImpl(
 ) : CatsRepository {
 
     override suspend fun loadCats(limit: Int): List<CatModel> {
-        val result = api.getCats(10, 1)
+        return withContext(Dispatchers.IO) {
+            val result = api.getCats(10, 1)
 
-        return result.map(catModelMapper::map)
+            result.map(catModelMapper::map)
+        }
     }
 
     override suspend fun getFavouriteCats(): Flow<CatModel> {
-        return dao.getCatsWithBreeds().map(catModelMapper::map)
+        return withContext(Dispatchers.IO) {
+            dao.getCatsWithBreeds().map(catModelMapper::map)
+        }
     }
 
     override suspend fun deleteFavouriteCat(model: CatModel) {
-        val catEntity = CatEntity(
-            id = model.id,
-            url = model.url
-        )
+        withContext(Dispatchers.IO) {
+            val catEntity = CatEntity(
+                id = model.id,
+                url = model.url
+            )
 
-        dao.deleteCat(catEntity)
+            dao.deleteCat(catEntity)
+        }
     }
 
     override suspend fun insertFavouriteCat(model: CatModel) {
-        val catEntity = CatEntity(
-            id = model.id,
-            url = model.url
-        )
-
-        val breedsEntity = model.breeds.map {
-            BreedEntity(
-                id = it.id,
-                catId = model.id,
-                name = it.name,
-                temperament = it.temperament,
-                origin = it.origin,
-                description = it.description
+        withContext(Dispatchers.IO) {
+            val catEntity = CatEntity(
+                id = model.id,
+                url = model.url
             )
+
+            val breedsEntity = model.breeds.map {
+                BreedEntity(
+                    id = it.id,
+                    catId = model.id,
+                    name = it.name,
+                    temperament = it.temperament,
+                    origin = it.origin,
+                    description = it.description
+                )
+            }
+
+            val catWithBreedsEntity = CatWithBreedsEntity(catEntity, breedsEntity)
+
+            dao.insertCatWithBreeds(catWithBreedsEntity)
         }
-
-        val catWithBreedsEntity = CatWithBreedsEntity(catEntity, breedsEntity)
-
-        dao.insertCatWithBreeds(catWithBreedsEntity)
     }
 }
